baf0eb7df233f6935915303654a70a6c476317a7
nrsmac <noahrschill@gmail.com>
2018-02-28 12:57:51 -0700

    basic functionality

--- main.js
+++ main.js
@@ -9,13 +9,22 @@ function createGrid(size) {
     for (let x=0; x < size; x++) {
       let div = document.createElement('div');
       div.classList.add('boxItem');
-      div.style.gridColumnStart = x
+      div.style.gridColumnStart = x;
       div.style.gridColumnEnd = x+1;
-
       container.appendChild(div);
+      div.addEventListener('mouseover', changeColor);
 
     }
   }
 }
+
+function changeColor(e) {
+  e.target.style.backgroundColor = 'black';
+}
+
+// function addRandomBackgroundColor(e) {
+//   e.target.style.backgroundColor = randomColor();
+// }
+
 
 createGrid(16);
