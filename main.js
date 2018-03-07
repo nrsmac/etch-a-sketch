3c2c91411d8c35e1d3e793c0bd0b4111b971dc63
nrsmac <noahrschill@gmail.com>
2018-03-06 21:10:46 -0700

    final functionallity added

--- main.js
+++ main.js
@@ -5,15 +5,16 @@ const container = document.getElementById('container');
 
 
 function createGrid(size) {
+  let boxSize = size/600;
   for (let y=0; y < size; y++){
     for (let x=0; x < size; x++) {
       let div = document.createElement('div');
       div.classList.add('boxItem');
       div.style.gridColumnStart = x;
       div.style.gridColumnEnd = x+1;
+
       container.appendChild(div);
       div.addEventListener('mouseover', changeColor);
-
     }
   }
 }
@@ -22,9 +23,39 @@ function changeColor(e) {
   e.target.style.backgroundColor = 'black';
 }
 
+
+
 // function addRandomBackgroundColor(e) {
 //   e.target.style.backgroundColor = randomColor();
 // }
 
+function clearGrid(){
+  const clearButton = document.getElementById('clear');
+  clearButton.addEventListener('click', function(e) {
+    let size = prompt("Size:","16");
+    // if size > 64 {
+    //   alert("error! too big!");
+    //   break;
+    // }
+    deleteGrid();
+    createGrid(size);
+  })
+
+}
+
+
+function deleteGrid() {
+  let container = document.querySelector('#container');
+  Element.prototype.removeAll = function () {
+    while (this.firstChild) {
+      this.removeChild(this.firstChild);
+    }
+    return this;
+  }
+  container.removeAll();
+}
+
+
 
 createGrid(16);
+clearGrid();
